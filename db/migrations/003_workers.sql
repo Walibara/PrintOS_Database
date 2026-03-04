067f8b076e6d1c8ff186a57247b40123a39b2e5f
Walibara <56965416+Walibara@users.noreply.github.com>
2026-03-04 11:13:35 -0800

    Create workers table with relevant fields

--- db/migrations/003_workers.sql
+++ db/migrations/003_workers.sql
@@ -1,1 +1,9 @@
+-- Workers table
+CREATE TABLE IF NOT EXISTS workers (
+    id VARCHAR(50) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    lambda_function_name VARCHAR(255) NOT NULL,
+    behavior_type ENUM('SUCCESS','FAILURE','TIMEOUT') NOT NULL,
+    PRIMARY KEY (id)
+);
 
