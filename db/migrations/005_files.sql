0aa8627362a9e05b39e11ac9d5ded4dc09f64a95
Walibara <56965416+Walibara@users.noreply.github.com>
2026-03-13 11:24:56 -0700

    Fix formatting and add foreign key constraint

--- db/migrations/005_files.sql
+++ db/migrations/005_files.sql
@@ -4,7 +4,10 @@ CREATE TABLE IF NOT EXISTS files (
     original TINYINT(1) DEFAULT 0,
     file_link VARCHAR(1000) NOT NULL,
     created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_modified_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    last_modified_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(50),
-    PRIMARY KEY (file_name)
+    PRIMARY KEY (file_name),
+    CONSTRAINT fk_files_last_modified_by
+        FOREIGN KEY (last_modified_by) REFERENCES users(id)
 );
