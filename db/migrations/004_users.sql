0756d0965c009d55077be3c44003d958a1cb4af0
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2026-04-07 22:49:00 -0700

    Update 004_users.sql
    Updated users' table so it works with Cognito

--- db/migrations/004_users.sql
+++ db/migrations/004_users.sql
@@ -1,14 +1,18 @@
 -- Users table
 CREATE TABLE IF NOT EXISTS users (
-    id BIGINT AUTO_INCREMENT NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    cognito_sub VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL;
     name VARCHAR(255) NOT NULL,
+        
     created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    jobs VARCHAR(255),
+    /*jobs VARCHAR(255),
     password VARCHAR(255) NOT NULL,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(50),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id)*/ -- commenting this out since it's not needed. Maria 
 );
